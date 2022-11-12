ac95cc2f92b74ea9c3b6f332c5c86b617dc42ea4
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-11-12 18:25:22 +0530

    Employee Payroll App Program

---
+++ src/Model/employee.ts
@@ -0,0 +1,26 @@
+
+// this is our model class having same field as we provide in the backend 
+export class Employee {
+
+    firstName!: String;
+    lastName!: String;
+    gender!: String;
+    department!: String;
+    salary!: number;
+    notes!: String
+    profilePic!: String;
+    date!: String;
+
+    constructor(
+        firstName: String,
+        lastName:String,
+        gender: String,
+        department: String,
+        salary: number,
+        notes: String,
+        profilePic:String,
+        date:String
+        ) 
+        { }
+
+}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Employee-App'`, () => {
+  it(`should have as title 'EmployeePayroll'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Employee-App');
+    expect(app.title).toEqual('EmployeePayroll');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Employee-App app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('EmployeePayroll app is running!');
   });
 });
